6012f93eb494e6a2850f1a8b69a77fb47ba6d391
EC2 Default User <ec2-user@ip-172-31-36-67.ec2.internal>
2020-11-17 02:57:00 +0000

    tiny change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
-  devise_for :members
   root 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
+  ActiveAdmin.routes(self)
+  devise_for :members
 end
