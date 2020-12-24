6f60eff597d5281ad6de8c6af320d4e68bcd747a
EC2 Default User <ec2-user@ip-172-31-36-67.ec2.internal>
2020-12-24 12:45:46 +0000

    modify image

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,6 +6,8 @@ class Member < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   attachment :image
+  #mount_uploader :image_id, ImageUploader
+
   enum role: { member: 0, admin: 1 }
   validates :name, presence: true, length: {maximum: 20, minimum: 1, allow_blank: true}
   validates :name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。', allow_blank: true}
